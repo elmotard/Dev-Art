4e42dfa7a6a15df50cede5d01d5f709e7c4ea19b
elmotard <elmotard@users.noreply.github.com>
2017-06-22 13:52:58 +0200

    Ajout du son

--- Detente/mainwindow.cpp
+++ Detente/mainwindow.cpp
@@ -1,21 +1,35 @@
 #include "mainwindow.h"
-#include <QFrame>
 
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
 {
+	listnomboutton << "Mer/Océan" << "Forêt" << "Montagne" << "Millieu Urbain" << "Général";
     mainlayout = new QVBoxLayout;
     layout1 = new QHBoxLayout;
     layout2 = new QHBoxLayout;
     TPBouton = new QPushButton*[nbBouton];
+	QMediaPlayer *son;
+	son=new QMediaPlayer();
+
+	son->setMedia(QMediaContent(QUrl::fromLocalFile("1.mp3")));
+	son->setVolume(50);
+	son->play();
+
     for(unsigned int i=0; i<=nbBouton; ++i)
     {
-        TPBouton[i]= new QPushButton("Je suis un bouton");
+		TPBouton[i]= new QPushButton(listnomboutton[i]);
         if(i <= 1)
             layout1->addWidget(TPBouton[i]);
         if(i>1  && i<=3)
             layout2->addWidget(TPBouton[i]);
     }
+
+	connect(TPBouton[0], &QPushButton::clicked, this, &MainWindow::mer);
+	connect(TPBouton[1], &QPushButton::clicked, this, &MainWindow::foret);
+	connect(TPBouton[2], &QPushButton::clicked, this, &MainWindow::montagne);
+	connect(TPBouton[3], &QPushButton::clicked, this, &MainWindow::urbain);
+	connect(TPBouton[4], &QPushButton::clicked, this, &MainWindow::general);
+
     mainlayout->addLayout(layout1);
     mainlayout->addLayout(layout2);
     mainlayout->addWidget(TPBouton[nbBouton]);
@@ -30,6 +44,31 @@ MainWindow::MainWindow(QWidget *parent)
 }
 
 MainWindow::~MainWindow()
+{
+
+}
+
+void MainWindow::mer()
+{
+
+}
+
+void MainWindow::foret()
+{
+
+}
+
+void MainWindow::montagne()
+{
+
+}
+
+void MainWindow::urbain()
+{
+
+}
+
+void MainWindow::general()
 {
 
 }
